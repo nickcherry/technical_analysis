240a4757e08600ba18e21a6a85f596e20090aeed
Nick Cherry <ncherry@cipherhealth.com>
2018-02-03 18:47:47 -0500

    prioritize fetching of long-duration candles and move non-directory-related fetch settings to fetch.js

--- bin/fetch.js
+++ bin/fetch.js
@@ -8,36 +8,36 @@ const chalk = require('chalk');
 const fs = require('fs');
 const gdax = require('gdax');
 const moment = require('moment');
-const { defaultPriceHistoryStartDate, defaultProduct, priceHistoryDir } = require('../settings');
+const { priceHistoryDir } = require('../settings');
 
 
 /******************************************************************************/
 /* Constants / Settings */
 /******************************************************************************/
 
-const PRODUCT = argv.product || defaultProduct;
-const START_TIME = Object.freeze(moment(argv['start-time'] || defaultPriceHistoryStartDate));
+const PRODUCT = argv.product || 'BTC-USD';
+const START_TIME = Object.freeze(moment(argv['start-time'] || new Date(2016, 0, 1).getTime()));
 const END_TIME = Object.freeze(moment(argv['end-time']));
 const CANDLE_SIZE = argv['candle-size'];
 
 const CANDLE_SIZE_ALL = 'all';
-const CANDLE_SIZE_1_MINUTE = '1-minute';
-const CANDLE_SIZE_5_MINUTE = '5-minute';
-const CANDLE_SIZE_15_MINUTE = '15-minute';
-const CANDLE_SIZE_1_HOUR = '1-hour';
-const CANDLE_SIZE_6_HOUR = '6-hour';
 const CANDLE_SIZE_1_DAY = '1-day';
+const CANDLE_SIZE_6_HOUR = '6-hour';
+const CANDLE_SIZE_1_HOUR = '1-hour';
+const CANDLE_SIZE_15_MINUTE = '15-minute';
+const CANDLE_SIZE_5_MINUTE = '5-minute';
+const CANDLE_SIZE_1_MINUTE = '1-minute';
 
 // https://docs.gdax.com/#get-historic-rates
 const GDAX_MAX_DATA_POINTS = 350;
 const API_REQUEST_DELAY = 2000; // milliseconds
 
-const CONFIG_1_MINUTE_CANDLE = { name: '1-minute', granularity: 60 };
-const CONFIG_5_MINUTE_CANDLE = { name: '5-minute', granularity: 300 };
-const CONFIG_15_MINUTE_CANDLE = { name: '15-minute', granularity: 900 };
-const CONFIG_1_HOUR_CANDLE = { name: '1-hour', granularity: 3600 };
-const CONFIG_6_HOUR_CANDLE = { name: '6-hour', granularity: 21600 };
 const CONFIG_1_DAY_CANDLE =  { name: '1-day', granularity: 86400 };
+const CONFIG_6_HOUR_CANDLE = { name: '6-hour', granularity: 21600 };
+const CONFIG_1_HOUR_CANDLE = { name: '1-hour', granularity: 3600 };
+const CONFIG_15_MINUTE_CANDLE = { name: '15-minute', granularity: 900 };
+const CONFIG_5_MINUTE_CANDLE = { name: '5-minute', granularity: 300 };
+const CONFIG_1_MINUTE_CANDLE = { name: '1-minute', granularity: 60 };
 
 
 /******************************************************************************/
@@ -89,31 +89,31 @@ const fetch = (granularity, callback) => {
 const configs = [];
 
 switch(CANDLE_SIZE) {
-  case CANDLE_SIZE_1_MINUTE: configs.push(CONFIG_1_MINUTE_CANDLE); break;
-  case CANDLE_SIZE_5_MINUTE: configs.push(CONFIG_5_MINUTE_CANDLE); break;
-  case CANDLE_SIZE_15_MINUTE: configs.push(CONFIG_15_MINUTE_CANDLE); break;
-  case CANDLE_SIZE_1_HOUR: configs.push(CONFIG_1_HOUR_CANDLE); break;
-  case CANDLE_SIZE_6_HOUR: configs.push(CONFIG_6_HOUR_CANDLE); break;
   case CANDLE_SIZE_1_DAY: configs.push(CONFIG_1_DAY_CANDLE); break;
+  case CANDLE_SIZE_6_HOUR: configs.push(CONFIG_6_HOUR_CANDLE); break;
+  case CANDLE_SIZE_1_HOUR: configs.push(CONFIG_1_HOUR_CANDLE); break;
+  case CANDLE_SIZE_15_MINUTE: configs.push(CONFIG_15_MINUTE_CANDLE); break;
+  case CANDLE_SIZE_5_MINUTE: configs.push(CONFIG_5_MINUTE_CANDLE); break;
+  case CANDLE_SIZE_1_MINUTE: configs.push(CONFIG_1_MINUTE_CANDLE); break;
   case CANDLE_SIZE_ALL:
     configs.push(
-      CONFIG_1_MINUTE_CANDLE,
-      CONFIG_5_MINUTE_CANDLE,
-      CONFIG_15_MINUTE_CANDLE,
-      CONFIG_1_HOUR_CANDLE,
-      CONFIG_6_HOUR_CANDLE,
       CONFIG_1_DAY_CANDLE,
+      CONFIG_6_HOUR_CANDLE,
+      CONFIG_1_HOUR_CANDLE,
+      CONFIG_15_MINUTE_CANDLE,
+      CONFIG_5_MINUTE_CANDLE,
+      CONFIG_1_MINUTE_CANDLE,
     );
     break;
   default:
     const available = [
       CANDLE_SIZE_ALL,
-      CANDLE_SIZE_1_MINUTE,
-      CANDLE_SIZE_5_MINUTE,
-      CANDLE_SIZE_15_MINUTE,
-      CANDLE_SIZE_1_HOUR,
-      CANDLE_SIZE_6_HOUR,
       CANDLE_SIZE_1_DAY,
+      CANDLE_SIZE_6_HOUR,
+      CANDLE_SIZE_1_HOUR,
+      CANDLE_SIZE_15_MINUTE,
+      CANDLE_SIZE_5_MINUTE,
+      CANDLE_SIZE_1_MINUTE,
     ];
     console.error(chalk.red(`The provided candle-size ("${ CANDLE_SIZE }") is not recognized.`));
     console.error(chalk.red(`Valid arguments include: ${ available.join(', ') } `));
